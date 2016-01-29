17866b06082fc9d9abaa1752999f1b5c964c7b8c
Victor Purcallas <vpurcallas@gmail.com>
2016-01-29 09:12:16 +0100

    Add basic fragments. Add util connectivity class

---
+++ app/src/main/java/org/safegees/safegees/util/Connectivity.java
@@ -0,0 +1,117 @@
+package org.safegees.safegees.util;
+
+import android.net.ConnectivityManager;
+import android.content.Context;
+import android.net.NetworkInfo;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
+/**
+ * Connectivity
+ *
+ * Dectect if device is connected to internet, and if this connection is trought wifi or mobile brand
+ *
+ * Created by victor on 28/1/16.
+ */
+
+public class Connectivity {
+    private static int TIME_OUT = 10;
+    private static String SAFEGEES_SERVER_URL = "http://safegees.appspot.com/";
+
+    /**
+     * Is network avaiable
+     *
+     * @param context (android)
+     * @return true if the device has internet connection
+     */
+    public static boolean isNetworkAvaiable(Context context){
+        ConnectivityManager cm;
+        NetworkInfo info = null;
+        try {
+            cm = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+            info = cm.getActiveNetworkInfo();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        if (info != null) return true;
+        else return false;
+
+    }
+
+    /**
+     * Maybe is not allways working.
+     *
+     * "This method first tries to use ICMP (ICMP ECHO REQUEST). When first step fails, a TCP connection on
+     * port 7 (Echo) of the remote host is established." I have found that many Android devices don't support ICMP,
+     * and many servers don't accept TCP connections over port 7. isReachable() then simply times out while the
+     * server is reachable.
+     */
+    /**
+     * Is SafegeesServer Avaiable
+     *
+     * @param context (android)
+     * @return  true if the mobile can connect with the Safegees Server
+     */
+    public static boolean isSafegeesServerAvaiable(Context context){
+        if (Connectivity.isNetworkAvaiable(context)){
+            try {
+                return InetAddress.getByName(SAFEGEES_SERVER_URL).isReachable(TIME_OUT);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Is Wifi Connection in the device
+     *
+     * @param context (android)
+     * @return true if the device is conected trought wifi
+     */
+    public static boolean isWifiConnection(Context context){
+        if (Connectivity.isNetworkAvaiable(context)) {
+            ConnectivityManager cm;
+            NetworkInfo info = null;
+            try {
+                cm = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+                info = cm.getActiveNetworkInfo();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (info != null) {
+                return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Is Mobile Connection, detects if the device is connected by mobile brand
+     * @param context (android)
+     * @return true if the device is connected by mobile brand.
+     */
+    public static boolean isMobileConnection(Context context){
+        if (Connectivity.isNetworkAvaiable(context)) {
+            ConnectivityManager cm;
+            NetworkInfo info = null;
+            try {
+                cm = (ConnectivityManager) context.getSystemService(context.CONNECTIVITY_SERVICE);
+                info = cm.getActiveNetworkInfo();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            if (info != null) {
+                return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
+            }
+        }
+        return false;
+    }
+
+}
---
+++ app/src/main/java/org/safegees/safegees/util/MemoryManager.java
@@ -0,0 +1,77 @@
+/**
+ * Memory Manager
+ *
+ * This class allows to store in Shared Preferences memory some kind of data.
+ * This was made to store String, int, boolean, and long values with a String key as identificator
+ *
+ * Created by victor on 28/1/16.
+ */
+
+package org.safegees.safegees.util;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class MemoryManager {
+    private SharedPreferences config;
+    private SharedPreferences.Editor configEditor;
+
+    public MemoryManager(Activity _activity) {
+        config =  _activity.getPreferences(Context.MODE_PRIVATE);
+        configEditor = config.edit();
+    }
+
+    public void putString(String key, String value) {
+        configEditor.putString(key, value);
+        configEditor.commit();
+    }
+
+    public void putLong(String key, long value) {
+        configEditor.putLong(key, value);
+        configEditor.commit();
+    }
+
+    public void putInt(String key, int value) {
+        configEditor.putInt(key, value);
+        configEditor.commit();
+    }
+
+    public void putBoolean(String key, boolean value) {
+        configEditor.putBoolean(key, value);
+        configEditor.commit();
+    }
+
+
+    public String getString(String key) {
+        return config.getString(key, "");
+    }
+
+    public long getLong(String key) {
+        return config.getLong(key, 0);
+    }
+
+    public int getInt(String key) {
+        return config.getInt(key, 0);
+    }
+
+    public boolean getBoolean(String key) {
+        return config.getBoolean(key, false);
+    }
+
+
+    public boolean contains(String key) {
+        return config.contains(key);
+    }
+
+    public void remove(String key) {
+        configEditor.remove(key);
+        configEditor.commit();
+    }
+
+    public void clearAll() {
+        configEditor.clear();
+        configEditor.commit();
+    }
+
+}
