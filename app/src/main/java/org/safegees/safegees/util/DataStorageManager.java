412ec17f6c64e02f1f9233c9ae1c6884fd73b9a3
Victor Purcallas <vpurcallas@gmail.com>
2016-02-09 11:44:00 +0100

    Add Licenses to .java classes

--- app/src/main/java/org/safegees/safegees/util/DataStorageManager.java
+++ app/src/main/java/org/safegees/safegees/util/DataStorageManager.java
@@ -1,10 +1,24 @@
 /**
- * Data Storage Manager
+ *   DataStorageManager.java
  *
- * This class allows to store in Shared Preferences memory some kind of data.
- * This was made to store String, int, boolean, and long values with a String key as identificator
+ *   This class allows to store in Shared Preferences memory some kind of data.
+ *   This was made to store String, int, boolean, and long values with a String key as identificator
  *
- * Created by victor on 28/1/16.
+ *   Copyright (C) 2016  Victor Purcallas <vpurcallas@gmail.com>
+ *
+ *   Safegees is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Safegees is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with ARcowabungaproject.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package org.safegees.safegees.util;
--- app/src/main/java/org/safegees/safegees/util/NetworkStateReceiver.java
+++ app/src/main/java/org/safegees/safegees/util/NetworkStateReceiver.java
@@ -1,3 +1,26 @@
+/**
+ *   NetworkStateReceiver.java
+ *
+ *   Future class description
+ *
+ *
+ *   Copyright (C) 2016  Victor Purcallas <vpurcallas@gmail.com>
+ *
+ *   Safegees is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Safegees is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with ARcowabungaproject.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.safegees.safegees.util;
 
 import android.content.BroadcastReceiver;
